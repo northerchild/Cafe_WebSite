21c6f803030ab79637223f481544e47e0a6df5f8
Northerchild <osarus-13@hotmail.com>
2017-10-25 17:10:19 -0500

    Se agrega las animaciones al header

---
+++ js/efecto.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+//Efecto menu//
+
+	$('.menu a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+		$(this).animate({
+			top:0
+		}, 2000 + (index *500));
+	});
+
+//Fin Efecto menu//
+
+//Efecto Header//
+	
+	if ($(window).width() > 800) 
+	{
+		$('header .textos').css({
+			opacity: 0,
+			marginTop:0
+		});
+		$('header .textos').animate({
+			opacity: 1,
+			marginTop:"-52px"
+		}, 1500);
+	}
+//Fin Efecto Header//
+
+});
